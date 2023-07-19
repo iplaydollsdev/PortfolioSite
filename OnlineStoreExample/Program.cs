d35d02d7cc8ccbf75a1db73725334d5607f82e99
iplaydollsdev <iplaydollsdev@gmail.com>
2023-07-19 15:52:32 +0300

    Add awaiter

--- OnlineStoreExample/Program.cs
+++ OnlineStoreExample/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 if (!builder.Environment.IsDevelopment())
 {
    string connectionString = System.IO.File.ReadAllText("connectionstring.txt");
+   await Task.Delay(1000);
 
    // Add services to the container.
    builder.Services.AddDbContext<AppDbContext>
