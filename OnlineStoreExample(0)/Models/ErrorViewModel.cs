b7fe59460e366eda3f099a58134ecaf9c9165005
Maxim Gulin <65855797+iplaydollsdev@users.noreply.github.com>
2023-07-19 14:57:56 +0300

    Delete OnlineStoreExample(0) directory

--- OnlineStoreExample(0)/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace OnlineStoreExample.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
